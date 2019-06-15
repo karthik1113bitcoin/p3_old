421e9fb453bc3dd50c371f6a4bc55a7377ed7a74
karthik1113bitcoin <50091765+karthik1113bitcoin@users.noreply.github.com>
2019-06-15 18:42:44 +0800

    Update reorg.sh

--- reorg.sh
+++ reorg.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 cp /tmp/cust/COMMON_CNTRY/COMMON_MODULE/scripts/* /tmp/ORG/cust/INFENG/scripts/
+echo "curl https://docs.openshift.com/enterprise/3.2/admin_guide/securing_builds.html" >> /tmp/TOOL/trigger.sh
